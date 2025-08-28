be9fb5a054c65c2c2d75b68c4ea404f03f56c8c1
Erik R <erik.riklund@outlook.com>
2025-08-28 21:28:36 +0200

    added comments

--- template-compiler/index.ts
+++ template-compiler/index.ts
@@ -7,7 +7,7 @@ import type { RenderFunction } from './types'
 import { createPipeline } from 'composable-pipeline'
 
 //
-// ?
+// The stages of the template compiler, each isolated and composable.
 //
 import { parse } from './stages/parse'
 import { transform } from './stages/transform'
@@ -16,20 +16,15 @@ import { outputToString } from './stages/output/string'
 import { outputToFunction } from './stages/output/function'
 
 //
-// ?
+// The template compiler module, providing two entry points: `toString` and `toFunction`.
+// These create a render function, either stringified or executable.
 //
 export const compileTemplate =
 {
-  //
-  // ?
-  //
   toString: createPipeline<string, string>(
     [parse, transform, compile, outputToString]
   ),
 
-  //
-  // ?
-  //
   toFunction: createPipeline<string, RenderFunction>(
     [parse, transform, compile, outputToFunction]
   )
