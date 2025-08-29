27912d5a350d1aae1191c180209afcd61e5b4334
Erik R <erik.riklund@outlook.com>
2025-08-29 07:06:40 +0200

    remove helpers

--- template-compiler/tests/helpers/match-pattern.test.ts
+++
@@ -1,9 +0,0 @@
-//
-// Copyright 2025 Erik Riklund (Gopher)
-// <https://github.com/erik-riklund>
-//
-
-import { it, expect } from 'bun:test'
-import { matchPattern } from 'template-compiler/helpers'
-
-// ---
