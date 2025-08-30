076e9688d322c9b55bd04702a08097fa5c08a69e
Erik R <erik.riklund@outlook.com>
2025-08-30 10:01:13 +0200

    remove "template-compiler"

--- template-compiler/tests/transform.test.ts
+++
@@ -1,9 +0,0 @@
-//
-// Copyright 2025 Erik Riklund (Gopher)
-// <https://github.com/erik-riklund>
-//
-
-import { it, expect } from 'bun:test'
-import { transform } from '../stages/transform'
-
-// ---
