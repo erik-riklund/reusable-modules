3b11327d339626870ba8c8b81f0d363c0405d679
Erik R <erik.riklund@outlook.com>
2025-09-04 09:51:08 +0200

    remove comments

--- task-manager/types.d.ts
+++ task-manager/types.d.ts
@@ -3,11 +3,8 @@
 // <https://github.com/erik-riklund>
 //
 
-//
-// The contract for the store that can be used to share data between tasks.
-//
 export interface Store
 {
-  getValue: <T> (name: string) => T,
-  setValue: (name: string, value: unknown) => void
+  getValue: <T> (name: string) => T;
+  setValue: (name: string, value: unknown) => void;
 }
