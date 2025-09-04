6f4e32c2784c2b6ff9a738a49cf936bbf8cac962
Erik R <erik.riklund@outlook.com>
2025-09-04 09:50:32 +0200

    rework shared types

--- types.d.ts
+++ types.d.ts
@@ -1,40 +1,17 @@
+//
+// Copyright 2025 Erik Riklund (Gopher)
+// <https://github.com/erik-riklund>
+//
+
 export {};
 
 declare global
 {
-  /**
-   * Represents a value that could be a `Promise`.
-   */
-  type Awaitable<T> = T | Promise<T>;
-
-  /**
-   * Represents an array of JSON values.
-   */
   type JsonArray = Array<JsonValue>;
-
-  /**
-   * Represents an object with string keys and JSON values.
-   */
   type JsonObject = { [key: string]: JsonValue; };
+  type JsonValue = string | number | boolean | null | JsonObject | JsonArray;
 
-  /**
-   * Represents a JSON value.
-   */
-  type JsonValue =
-    | string
-    | number
-    | boolean
-    | null
-    | JsonObject
-    | JsonArray;
-
-  /**
-   * Represents a value that could be `null`.
-   */
+  type MaybePromise<T> = T | Promise<T>;
   type Nullable<T> = T | null;
-
-  /**
-   * Represents a value that could be `undefined`.
-   */
   type Optional<T> = T | undefined;
 }
