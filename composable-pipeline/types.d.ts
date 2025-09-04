5b7e61b77fedf21b616f86dfb7260f617f41bfde
Erik R <erik.riklund@outlook.com>
2025-09-04 09:50:54 +0200

    remove comments

--- composable-pipeline/types.d.ts
+++ composable-pipeline/types.d.ts
@@ -3,12 +3,5 @@
 // <https://github.com/erik-riklund>
 //
 
-//
-// An asyncronous stage in a sequential pipeline.
-//
 type Stage<I, R> = (input: I) => Promise<R>;
-
-//
-// A list of stages in a sequential pipeline.
-//
 export type Stages = Array<Stage<any, any>>;
