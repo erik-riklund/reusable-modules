1d0b34a4a69e739b345c5d9e0edd1ce4c5489c01
Erik R <erik.riklund@outlook.com>
2025-09-13 23:11:36 +0200

    add build engine

---
+++ css-build-engine/index.ts
@@ -0,0 +1,37 @@
+//
+// Created by Erik Riklund (Gopher) 2025
+// <https://github.com/erik-riklund>
+//
+// @version 1.0.0
+//
+
+import type {
+  InputPlugin as PipelineInputPlugin,
+  TransformPlugin as PipelineTransformPlugin,
+  OutputPlugin as PipelineOutputPlugin
+} from 'css-build-pipeline'
+
+import { createBuildPipeline } from 'css-build-pipeline'
+
+// ---
+
+type Plugins = Array<InputPlugin | TransformPlugin | OutputPlugin>;
+
+export type InputPlugin = { type: 'input', plugin: PipelineInputPlugin };
+export type TransformPlugin = { type: 'transform', plugin: PipelineTransformPlugin };
+export type OutputPlugin = { type: 'output', plugin: PipelineOutputPlugin };
+
+// ---
+
+export function createBuildEngine (plugins: Plugins = [])
+{
+  const pipeline = createBuildPipeline(
+    {
+      input: plugins.filter((plugin) => plugin.type === 'input').map(({ plugin }) => plugin),
+      transform: plugins.filter((plugin) => plugin.type === 'transform').map(({ plugin }) => plugin),
+      output: plugins.filter((plugin) => plugin.type === 'output').map(({ plugin }) => plugin)
+    }
+  );
+
+  return (input: string): Promise<string> => pipeline.run(input);
+}
